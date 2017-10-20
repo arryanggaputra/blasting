83b302ca2c95f808675aac70137adc2218bfb94a
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-10-20 08:30:38 +0700

    Change some helper functionality

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -7,8 +7,7 @@ if (!function_exists('request')) {
      */
     function request()
     {
-        $request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
-        return $request;
+        return Zend\Diactoros\ServerRequestFactory::fromGlobals();
     }
 }
 
@@ -20,40 +19,39 @@ if (!function_exists('response')) {
      */
     function response()
     {
-        $response = new Symfony\Component\HttpFoundation\Response;
-        return $response;
+        return (new Zend\Diactoros\Response);
     }
 }
 
-if (!function_exists('responseJson')) {
+if (!function_exists('JsonResponse')) {
     /**
      * Response represents an HTTP response in JSON format
      * @return Symfony\Component\HttpFoundation\Response
      */
-    function responseJson($data = null)
+    function JsonResponse($data = null)
     {
-        if (is_array($data)) {
-            $data = json_encode($data);
-        }
+        return new Zend\Diactoros\Response\JsonResponse($data);
+    }
+}
 
-        $response = response();
-        $response->setContent($data);
-        $response->headers->set('Content-Type', 'application/json');
-        $response->send();
+if (!function_exists('root_path')) {
+    function root_path(string $path = '')
+    {
+        return __DIR__ . '/../' . $path;
     }
 }
 
 if (!function_exists('storage_path')) {
     function storage_path(string $path = '')
     {
-        return __DIR__ . '/../storage/' . $path;
+        return root_path('storage/' . $path);
     }
 }
 
 if (!function_exists('resource_path')) {
     function resource_path(string $path = '')
     {
-        return __DIR__ . '/../resources/' . $path;
+        return root_path('resources/' . $path);
     }
 }
 
@@ -61,5 +59,25 @@ if (!function_exists('view_path')) {
     function view_path(string $path = '')
     {
         return resource_path('views/' . $path);
+    }
+}
+
+if (!function_exists('app')) {
+    function app()
+    {
+        $dependencies = require_once __DIR__ . '/../config/dependency.php';
+        $container    = new League\Container\Container;
+        foreach ($dependencies as $interface => $implementation) {
+            $container->share($interface, $implementation);
+        }
+        return $container;
+    }
+}
+
+if (!function_exists('view')) {
+    function view(string $path, array $data = [])
+    {
+        $view = app()->get('Systemblast\Interfaces\View');
+        return new Zend\Diactoros\Response\HtmlResponse($view->render($path, $data));
     }
 }
