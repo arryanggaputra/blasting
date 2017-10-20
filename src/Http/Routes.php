4890db32ad5dab66c59c9cecf6e3ba10d0aa13a1
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-10-20 08:33:28 +0700

    Change route implementation using League/Route

--- src/Http/Routes.php
+++ src/Http/Routes.php
@@ -1,33 +1,10 @@
 <?php
 
-namespace Blasting\Http;
-
-use Phroute\Phroute\RouteCollector;
-
 /**
  * Routes
  */
-final class Routes
-{
-    public $router;
+$route = new League\Route\RouteCollection;
 
-    public function __construct()
-    {
-        $this->router = new RouteCollector;
-    }
+$route->get('/', '\Blasting\Controllers\HomeController::index');
 
-    public function __invoke()
-    {
-        // Check middleware size, and register middleware
-        if (sizeof((new Layers)()) > 0) {
-            foreach ((new Layers)() as $key => $fqcn) {
-                $this->router->filter($key, function () use ($fqcn) {
-                    return (new $fqcn)->handle();
-                });
-            }
-        }
-
-        $this->router->get('/', [\Blasting\Controllers\HomeController::class, 'index']);
-        return $this->router->getData();
-    }
-}
+return $route;
