db0e082afaaf5cc3c4c141d9c73a3ab907b41954
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-10-23 06:43:40 +0700

    Add auto_reload option on Twig

--- system/Engine/Template/Twig.php
+++ system/Engine/Template/Twig.php
@@ -18,7 +18,8 @@ class Twig implements ViewInterface
     {
         $loader     = new \Twig_Loader_Filesystem(view_path());
         $this->view = new \Twig_Environment($loader, [
-            'cache' => storage_path('views/twig'),
+            'cache'       => storage_path('views/twig'),
+            'auto_reload' => (getenv('APP_ENV') == 'local') ? true : false,
         ]);
     }
 
