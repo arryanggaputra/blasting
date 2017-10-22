eb7b698f89d42276f6db7f623daef26f49fcb8b0
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-10-22 21:09:20 +0700

    Fix HelperTest

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -9,10 +9,20 @@ class HelperTest extends TestCase
 {
     public function testRequestHelper()
     {
-        $this->assertInstanceOf(Symfony\Component\HttpFoundation\Request::class, request());
-        $this->assertInstanceOf(Symfony\Component\HttpFoundation\Response::class, response());
+        $this->assertTrue(function_exists('request'));
+        $this->assertTrue(function_exists('response'));
+        $this->assertTrue(function_exists('json_response'));
+        $this->assertTrue(function_exists('root_path'));
+        $this->assertTrue(function_exists('storage_path'));
+        $this->assertTrue(function_exists('view_path'));
+        $this->assertTrue(function_exists('app'));
+        $this->assertTrue(function_exists('view'));
+        $this->assertTrue(function_exists('input'));
 
-        $this->markTestIncomplete();
+        $this->assertInstanceOf(Zend\Diactoros\ServerRequest::class, request());
+        $this->assertInstanceOf(Zend\Diactoros\Response::class, response());
+        $this->assertInstanceOf(Zend\Diactoros\Response\JsonResponse::class, json_response());
+        $this->assertInstanceOf(League\Container\Container::class, app());
 
     }
 }
