45cd424bf05601534478b503890cbf6442957c54
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-10-24 17:05:07 +0700

    Add Systemblast\Engine\Http\Input to catch all http input request

---
+++ system/Engine/Http/Input.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Systemblast\Engine\Http;
+
+/**
+ *
+ */
+class Input
+{
+    protected $request;
+
+    protected $jsonData;
+
+    protected $fromUrlencodedData;
+
+    public function __construct()
+    {
+        $this->request            = request();
+        $this->jsonData           = [];
+        $this->fromUrlencodedData = [];
+    }
+
+    public function getAll()
+    {
+        return $this->combineAllData();
+    }
+
+    public function get(string $key = null, $default = null)
+    {
+        return $this->getAll()[$key] ?? $default;
+    }
+
+    private function combineAllData()
+    {
+        // if http method is PUT and content-type is application/x-www-form-urlencoded
+        // we need to parse the string as an array
+        if (
+            $this->request->getMethod() == 'PUT' &&
+            in_array('application/x-www-form-urlencoded', $this->request->getHeader('content-type'))
+        ) {
+            parse_str($this->request->getBody()->getContents(), $this->fromUrlencodedData);
+
+        }
+
+        // if header content-type is application/json, we need to decode the json input
+        if (in_array('application/json', $this->request->getHeader('content-type'))) {
+            $jsonData       = json_decode($this->request->getBody()->getContents(), true);
+            $this->jsonData = ($jsonData !== null) ? $jsonData : $this->jsonData;
+        };
+
+        return array_merge_recursive(
+            $this->request->getParsedBody(),
+            $this->request->getQueryParams(),
+            $this->request->getUploadedFiles(),
+            $this->jsonData,
+            $this->fromUrlencodedData
+        );
+    }
+}
