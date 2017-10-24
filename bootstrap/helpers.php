9c97520eab1c1d05f5f125b931cb4622d05ba686
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-10-24 17:04:14 +0700

    - Router not working on localhost #3 - Fix input function - Fix helper code documentation

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,11 +3,22 @@
 if (!function_exists('request')) {
     /**
      * Get an instance of the current request or an input item from the request
+     *
      * @return Zend\Diactoros\ServerRequestFactory
      */
     function request()
     {
-        return Zend\Diactoros\ServerRequestFactory::fromGlobals();
+        /**
+         * to get the same $_SERVER['REQUEST_URI'] on both localhost and live server
+         */
+        if ($_SERVER['HTTP_HOST'] == 'localhost') {
+            $url                    = preg_replace('#^https?://#', '', $_SERVER['APP_URL']);
+            $url                    = str_replace($_SERVER['HTTP_HOST'], '', $url);
+            $_SERVER['REQUEST_URI'] = str_replace($url, '', $_SERVER['REQUEST_URI']);
+        }
+        return Zend\Diactoros\ServerRequestFactory::fromGlobals(
+            $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+        );
     }
 }
 
@@ -15,6 +26,7 @@ if (!function_exists('response')) {
     /**
      * A response function holds all the information that needs
      * to be sent back to the client from a given request
+     *
      * @return Zend\Diactoros\Response
      */
     function response()
@@ -26,15 +38,24 @@ if (!function_exists('response')) {
 if (!function_exists('json_response')) {
     /**
      * Response represents an HTTP response in JSON format
-     * @return Zend\Diactoros\Response\JsonResponse
+     *
+     * @param  null|array  $data data to represent on client
+     * @param  integer $code HTTP status code
+     * @return Zend\Diactoros\Response\JsonResponse JSON object
      */
-    function json_response($data = null)
+    function json_response($data = null, $code = 200)
     {
-        return new Zend\Diactoros\Response\JsonResponse($data);
+        return new Zend\Diactoros\Response\JsonResponse($data, $code);
     }
 }
 
 if (!function_exists('root_path')) {
+    /**
+     * get the rooth path
+     *
+     * @param  string $path path to add after the root path
+     * @return string
+     */
     function root_path(string $path = '')
     {
         return __DIR__ . '/../' . $path;
@@ -42,6 +63,12 @@ if (!function_exists('root_path')) {
 }
 
 if (!function_exists('storage_path')) {
+    /**
+     * get the storage path
+     *
+     * @param  string $path path to add after the storage path
+     * @return string
+     */
     function storage_path(string $path = '')
     {
         return root_path('storage/' . $path);
@@ -49,6 +76,12 @@ if (!function_exists('storage_path')) {
 }
 
 if (!function_exists('resource_path')) {
+    /**
+     * get the resource path
+     *
+     * @param  string $path path to add after the resource path
+     * @return string
+     */
     function resource_path(string $path = '')
     {
         return root_path('resources/' . $path);
@@ -56,6 +89,12 @@ if (!function_exists('resource_path')) {
 }
 
 if (!function_exists('view_path')) {
+    /**
+     * get the view path
+     *
+     * @param  string $path path to add after the view path
+     * @return string
+     */
     function view_path(string $path = '')
     {
         return resource_path('views/' . $path);
@@ -63,6 +102,11 @@ if (!function_exists('view_path')) {
 }
 
 if (!function_exists('app')) {
+    /**
+     * Get the available container instance.
+     *
+     * @return void
+     */
     function app()
     {
         $dependencies = require_once __DIR__ . '/../config/dependency.php';
@@ -75,6 +119,13 @@ if (!function_exists('app')) {
 }
 
 if (!function_exists('view')) {
+    /**
+     * Get the evaluated view contents for the given view
+     *
+     * @param  string $path path or view filename
+     * @param  array  $data data to represent to the client
+     * @return string
+     */
     function view(string $path, array $data = [])
     {
         $view = app()->get('Systemblast\Interfaces\View');
@@ -83,12 +134,13 @@ if (!function_exists('view')) {
 }
 
 if (!function_exists('input')) {
-    function input(string $key = null, $default = null)
+    /**
+     * Get an instance of the current request or an input item from the request.
+     *
+     * @return Systemblast\Engine\Http\Input|string|array
+     */
+    function input()
     {
-        $queryParams = request()->getQueryParams();
-        if ($key !== null) {
-            $queryParams = (isset($queryParams[$key])) ?: $default;
-        }
-        return $queryParams;
+        return new Systemblast\Engine\Http\Input;
     }
 }
