c76391f6a05c97441b5ad54667828c0ebd1d5139
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-10-27 08:16:32 +0700

    - Use env instead of getenv - Add env function

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -8,7 +8,7 @@ if (!function_exists('request')) {
      */
     function request()
     {
-        return app()->container->get('request');
+        return app()->container->get('request')::make();
     }
 }
 
@@ -109,7 +109,7 @@ if (!function_exists('view')) {
      *
      * @param  string $path path or view filename
      * @param  array  $data data to represent to the client
-     * @return string
+     * @return Zend\Diactoros\Response\HtmlResponse|string
      */
     function view(string $path, array $data = [])
     {
@@ -127,5 +127,43 @@ if (!function_exists('input')) {
     function input()
     {
         return app()->container->get('input');
+    }
+}
+
+if (!function_exists('env')) {
+    /**
+     * Gets the value of an environment variable. Supports boolean, empty and null.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+
+        if ($value === false) {
+            return $default;
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+
+            case 'false':
+            case '(false)':
+                return false;
+
+            case 'empty':
+            case '(empty)':
+                return '';
+
+            case 'null':
+            case '(null)':
+                return;
+        }
+
+        return $value;
     }
 }
